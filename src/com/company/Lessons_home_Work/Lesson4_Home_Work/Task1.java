63743cb7d39b6ae9625346c5fa40910ee3b0472e
Alexsander Tsova <tsova@bk.ru>
2016-08-18 23:26:59 +0300

    My change in the code

--- src/com/company/Lessons_home_Work/Lesson4_Home_Work/Task1.java
+++ src/com/company/Lessons_home_Work/Lesson4_Home_Work/Task1.java
@@ -1,14 +1,19 @@
 package com.company.Lessons_home_Work.Lesson4_Home_Work;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
 
 public class Task1 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         Scanner cr = new Scanner(System.in);
-        System.out.println("Please enter number in the binary system:");
-        String number2 = cr.nextLine();
+
         System.out.println("Please write a number in the decimal system:");
         int number = cr.nextInt();
+        System.out.println("Please enter number in the binary system:");
+        String number2 = br.readLine();
         System.out.println("Number in the decimal is " + number + " in the binary is " + Integer.toBinaryString(number));
         System.out.println("Number in the binary is " + number2 + " in the decimal is "+ Integer.parseInt(number2, 2));
     }
--- src/com/company/Lessons_home_Work/Lesson4_Home_Work/Task2.java
+++ src/com/company/Lessons_home_Work/Lesson4_Home_Work/Task2.java
@@ -1,14 +1,18 @@
 package com.company.Lessons_home_Work.Lesson4_Home_Work;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
 
 public class Task2 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException{
         Scanner cr = new Scanner(System.in);
-        System.out.println("Please write the umber in the hex system");
-        String number16 = cr.nextLine();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         System.out.println("Please write the umber in the decimal system");
         int number10 = cr.nextInt();
+        System.out.println("Please write the umber in the hex system");
+        String number16 = br.readLine();
         System.out.println("The number in the decimal is "+number10+" in the hex system is " + Integer.toHexString(number10));
         System.out.println("The number in the hex is "+number16+" in the decimal system is " + Integer.parseInt(number16, 16));
 

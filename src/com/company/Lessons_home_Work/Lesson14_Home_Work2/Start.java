5aa3e68068796fdb482df07a981799c508adf32e
Alexsander Tsova <tsova@bk.ru>
2016-09-19 21:54:47 +0300

    do change

--- src/com/company/Lessons_home_Work/Lesson14_Home_Work2/Start.java
+++ src/com/company/Lessons_home_Work/Lesson14_Home_Work2/Start.java
@@ -7,12 +7,17 @@ public class Start {
     public static void main(String[] args) {
         Url url = Url.newBuilder()
                 .setProtocol("https://")
-                .setdomen("google.com.ua")
-                .setpath("/search")
-                .setparam("?q=java")
-                .setparam("+builder")
+                .setDomen("google.com.ua")
+                .setPath("/search")
+                .setParam("?q=java")
+                .setParam("+builder")
                 .build();
 
         System.out.println(url);
+
+        Url url2 = Url.newBuilder()
+                .setParam("?q=java")
+                .build();
+        System.out.println(url2);
     }
 }
--- src/com/company/Lessons_home_Work/Lesson14_Home_Work2/Url.java
+++ src/com/company/Lessons_home_Work/Lesson14_Home_Work2/Url.java
@@ -1,9 +1,11 @@
 package com.company.Lessons_home_Work.Lesson14_Home_Work2;
 
 public class Url {
-    private String protocol;
-    private String domen;
-    private String path;
+    private String protocol = "http://";
+    private String subDomen = "";
+    private String domen = "google.com.ua";
+    private String port = "";
+    private String path = "/search";
     private String param = "";
 
     private Url(){    }
@@ -41,17 +43,27 @@ public class Url {
             return this;
         }
 
-        public builderUrl setdomen(String domen){
+        public builderUrl setSubDomen(String subDomen){
+            Url.this.subDomen = subDomen;
+            return this;
+        }
+
+        public builderUrl setDomen(String domen){
             Url.this.domen = domen;
             return this;
         }
 
-        public builderUrl setpath(String path){
+        public builderUrl setPort(String protocol){
+            Url.this.port = port;
+            return this;
+        }
+
+        public builderUrl setPath(String path){
             Url.this.path = path;
             return this;
         }
 
-        public builderUrl setparam(String param){
+        public builderUrl setParam(String param){
             Url.this.param += param;
             return this;
         }
